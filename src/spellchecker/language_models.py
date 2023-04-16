6efe171b4edd29f94cd699bbe289e92444d8a1e1
DenisRogozhin <den1s.rogozhin@mail.ru>
2023-04-16 21:51:28 +0300

    language model initial(#4)

---
+++ src/spellchecker/language_models.py
@@ -0,0 +1,13 @@
+from nltk import word_tokenize
+from collections import defaultdict
+
+class BigramLanguageModel: 
+
+    def __init__(self):
+        pass
+    
+    def fit(self, texts):
+        pass
+    
+    def P2(self, text, smoothing=None):
+        pass
