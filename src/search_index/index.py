26ba3188a3d73c4c3b3eca1135fae2efb13e8105
Aleksey Zubaryov <aleksej.zubaryov@gmail.com>
2023-05-05 00:48:43 +0300

    index class init (#10)

---
+++ src/search_index/index.py
@@ -0,0 +1,29 @@
+class Index:
+
+    def __init__(self):
+        pass
+
+
+    def build(self, text_list):
+        pass
+
+
+    def compress(self):
+        pass
+
+
+    def decompress(self):
+        pass
+
+
+    def search(self, query):
+        pass
+
+
+    def dump(self, index_filepath, compression_filepath):
+        pass
+
+
+    def load(self, index_filepath, compression_filepath):
+        pass
+    
