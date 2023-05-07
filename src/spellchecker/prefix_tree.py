eaccb9fd7dd58802357e460f9c95ab86a052bea8
DenisRogozhin <den1s.rogozhin@mail.ru>
2023-05-07 15:31:08 +0300

    add fit function(#11)

--- src/spellchecker/prefix_tree.py
+++ src/spellchecker/prefix_tree.py
@@ -1,10 +1,15 @@
+from utils import tokenize
+
 class Bor():
     
-    def __init__(self,error_model):
+    def __init__(self, error_model):
         pass
     
     def fit(self, correct_queries):
-        pass
+        for query in correct_queries:
+            query = tokenize(query)
+            for word in query:
+                self.add(word)
     
     def add(self, word):
         pass
