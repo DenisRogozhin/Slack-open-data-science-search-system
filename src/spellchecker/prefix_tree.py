545205edaef31010a9f6a6cf67ef64b9d303e710
DenisRogozhin <den1s.rogozhin@mail.ru>
2023-05-07 15:23:31 +0300

    add prefix tree intarface(#11)

---
+++ src/spellchecker/prefix_tree.py
@@ -0,0 +1,17 @@
+class Bor():
+    
+    def __init__(self,error_model):
+        pass
+    
+    def fit(self, correct_queries):
+        pass
+    
+    def add(self, word):
+        pass
+    
+    def get_prefix_freq(self, prefix):
+        pass
+    
+    def search(self, orig):
+        pass
+    
