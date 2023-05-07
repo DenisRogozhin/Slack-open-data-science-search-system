2fa314dc0d4f02b7b89aa3fcb7138f86923b4458
DenisRogozhin <den1s.rogozhin@mail.ru>
2023-05-07 16:21:24 +0300

    add word to prefix tree function(#11)

--- src/spellchecker/prefix_tree.py
+++ src/spellchecker/prefix_tree.py
@@ -12,7 +12,24 @@ class Bor():
                 self.add(word)
     
     def add(self, word):
-        pass
+        current = self.tree.children
+        n = len(word)
+        if n == 0:
+            return
+        for i in range(n - 1):
+            if word[i] not in current:
+                new_node = Node()
+                current[word[i]] = new_node
+            current[word[i]].freq += 1
+            current = current[word[i]].children
+        if word[n - 1] not in current:
+            new_node = Node(word)
+            current[word[n - 1]] = new_node
+            current[word[n - 1]].freq += 1
+        else:
+            current[word[n - 1]].freq += 1
+            if current[word[n - 1]].word is None:
+                current[word[n - 1]].word = word
     
     def get_prefix_freq(self, prefix):
         pass
