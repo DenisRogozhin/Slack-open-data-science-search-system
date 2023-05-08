f4de18d10922a3e47d339af0bbfbcfc0f36c8452
DenisRogozhin <den1s.rogozhin@mail.ru>
2023-05-09 01:30:47 +0300

    add fix split and grammar functions (#11)

--- src/spellchecker/spellchecker.py
+++ src/spellchecker/spellchecker.py
@@ -18,6 +18,42 @@ class SpellCorrector():
             join.extend(words[i + 2:])
             joins.append(join)
         return joins
+    
+    def fix_split(self, words):
+        splits = []
+        for i in range(len(words)):
+            word = words[i]
+            for j in range(1, len(word)):
+                split = words[0:i]
+                split.append(word[0:j])
+                split.append(word[j:])
+                split.extend(words[i + 1:])
+                splits.append(split)
+        return splits
+    
+    def grammar_error_correct(self, words):
+        list_of_candidates = []
+        for word in words:
+            candidates = list(map(lambda x: x[0], self.bor.search(word)))
+            list_of_candidates.append(candidates)
+        res = []
+        candidates1 = list_of_candidates[0]  
+        candidates1 = list(map(lambda x: [x], candidates1))  
+        for i in range(1,len(list_of_candidates)):
+            res = []
+            candidates2 = list_of_candidates[i]  
+            for word1 in candidates1: 
+                for word2 in candidates2:  
+                    cost = self.lm.P2(" ".join(word1 + [word2])) 
+                    if len(res) < self.max_candidates:
+                        res.append((word1 + [word2], cost))
+                    else:
+                        if cost > res[-1][1]:
+                            res[-1] = (word1 + [word2], cost)
+                    res = sorted(res, key = lambda x: x[1],reverse = True)
+            candidates1 = list(map(lambda x: x[0], res))
+                
+        return res
 
     def fix_layout(self, words):
         query = []
