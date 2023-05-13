fc89d9727ce0af87d3f6600e9a8988d18b34257d
Воронцов Александр Алексеевич <aavorontsov@domclick.ru>
2023-05-13 12:12:22 +0300

    Add docstrings for clean model (DenisRogozhin#13)

--- src/parser/clean.py
+++ src/parser/clean.py
@@ -1,12 +1,30 @@
+"""Realisation of Clean model."""
 import pandas as pd
 
 
 class Clean:
+    """Clean model."""
+
     def __init__(self, data_name: str = "data.csv", save: bool = True):
+        """Init Parser model.
+
+        :param data_name: name of raw data
+        :param save: bool flag for saving data
+        """
         self.data_name = data_name
         self.save = save
 
     def clean(self) -> pd.DataFrame:
+        """
+        Clean information from csv file.
+
+        1) Drop empty messages
+        2) Drop messages without users
+        3) Drop short messages
+        4) Drop messages with tech info (with <)
+        5) Drop messages with smiles (with :)
+        6) Drop messages that repeat more than 100 times
+        """
         df = pd.read_csv(self.data_name)
         df = df[~df["text"].isna()]
         df = df[~df["user"].isna()]
