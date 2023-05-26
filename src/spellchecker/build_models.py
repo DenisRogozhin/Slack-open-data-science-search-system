346ddd14fb664f846cd875ab482817e273666b7a
DenisRogozhin <den1s.rogozhin@mail.ru>
2023-05-27 00:57:16 +0300

    fix flake8 error in build_model (#11)

--- src/spellchecker/build_models.py
+++ src/spellchecker/build_models.py
@@ -7,10 +7,12 @@ import sys
 
 sys.setrecursionlimit(2000)
 
+
 def build_language_model(texts):
     model = BigramLanguageModel()
     model.fit(texts)
     pd.to_pickle(model, 'models/language_model.pickle')
+
 
 def build_error_model(fixed_texts):
     err = ErrorModel()
@@ -18,10 +20,12 @@ def build_error_model(fixed_texts):
     pd.to_pickle(err, 'models/error_model.pickle')
     return err
 
+
 def build_prefix_tree(err, texts):
     bor = Bor(err)
     bor.fit(texts)
     pd.to_pickle(bor, 'models/prefix_tree.pickle')
+
 
 def get_fixes(texts):
     res = []
@@ -31,13 +35,14 @@ def get_fixes(texts):
             res.append((tokenize(parts[0]), tokenize(parts[1])))
     return res
 
+
 if __name__ == "__main__":
     data = pd.read_csv('data.csv')
     texts = data.text.values
     build_language_model(texts)
-    #with open('queries_all.txt', 'r', encoding='utf-8') as f:
-    #    lines = f.read().split('\n')
-    #fixed_texts = get_fixes(lines)
-    #err = build_error_model(fixed_texts)
+#    with open('queries_all.txt', 'r', encoding='utf-8') as f:
+#        lines = f.read().split('\n')
+#    fixed_texts = get_fixes(lines)
+#    err = build_error_model(fixed_texts)
     err = pd.read_pickle('models/error_model.pickle')
     build_prefix_tree(err, texts)
