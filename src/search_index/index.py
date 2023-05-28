97c104f9026e3583ffce0bc1f3ac4f24193a5420
Aleksey Zubaryov <aleksej.zubaryov@gmail.com>
2023-05-28 13:14:06 +0300

    Add search for main post message and its dumps/loads

--- src/search_index/index.py
+++ src/search_index/index.py
@@ -1,24 +1,27 @@
 from collections import defaultdict
 import varbyte_encoding
+import pandas as pd
 import pickle
 
 
 class Index:
 
-    def __init__(self):
+    def __init__(self, filepath):
         self.index = defaultdict(list)
+        self.filepath = filepath
+        self.data = pd.read_csv(self.filepath, index_col=0).reset_index(drop=True)
 
 
-    def build(self, text_list=None):
-        text_id = 0
+    def build(self):
+        self.post_main_id = self.data.sort_values(by=['file', 'ts'], ascending=[True, True]).groupby('file').head(1)\
+                                    .reset_index().groupby('file').agg({'index': list})['index'].to_dict()        
 
-        for text in text_list:
-            tokens = text.split(' ')
+        for i, row in self.data.iterrows():
+            tokens = row['lemmatize_text'].split(' ')
 
             for tok in tokens:
-                self.index[tok].append(text_id)
-            text_id += 1
-
+                self.index[tok].append(i)
+        
         self.compress()
 
 
@@ -39,7 +42,15 @@ class Index:
         with open(filepath, mode='wb') as ind_f:
             pickle.dump(self.index, ind_f)
 
+        dot = filepath.rfind('.')
+        with open(filepath[:dot] + '_post' + filepath[dot:], mode='wb') as post_f:
+            pickle.dump(self.post_main_id, post_f)
+
 
     def load(self, filepath):
         with open(filepath, mode='rb') as ind_f:
             self.index = pickle.load(ind_f)
+        
+        dot = filepath.rfind('.')
+        with open(filepath[:dot] + '_post' + filepath[dot:], mode='rb') as post_f:
+            self.post_main_id = pickle.load(post_f)
