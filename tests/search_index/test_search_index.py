220ac7e9524636d9588c40dcbd87ed180c1d185b
Aleksey Zubaryov <aleksej.zubaryov@gmail.com>
2023-06-04 21:52:52 +0300

    Add tests for query processing (#10)

--- tests/search_index/test_search_index.py
+++ tests/search_index/test_search_index.py
@@ -1,7 +1,8 @@
 import bitstring
 import sys
-sys.path.append('../../src/search_index')
+sys.path.append('src/search_index')
 import varbyte_encoding
+import query_processing
 
 
 def test_add_byte():
@@ -28,3 +29,42 @@ def test_compress():
         varbyte_encoding.compress([1, 3, 4, 6])) == [1, 3, 4, 6]
     assert varbyte_encoding.decompress(
         varbyte_encoding.compress([1, 100, 102, 217])) == [1, 100, 102, 217]
+
+
+def test_tokenize():
+    assert query_processing.tokenize('1 & 2 & 3') == ['1', '&', '2', '&', '3']
+    assert query_processing.tokenize('(1 | 2) & (3 | 4)')\
+        == ['(', '1', '|', '2', ')', '&', '(', '3', '|', '4', ')']
+    assert query_processing.tokenize('((1 | 2) & 3) | 4')\
+        == ['(', '(', '1', '|', '2', ')', '&', '3', ')', '|', '4']
+    assert query_processing.tokenize('(!(1 | 2) & 3) | 4')\
+        == ['(', '!', '(', '1', '|', '2', ')', '&', '3', ')', '|', '4']
+
+
+def test_to_poliz():
+    assert query_processing.tokenized_query_to_poliz(
+        ['1', '&', '2', '&', '3']) == ['1', '2', '3', '&', '&']
+    assert query_processing.tokenized_query_to_poliz(
+        ['(', '1', '|', '2', ')', '&', '(', '3', '|', '4', ')'])\
+        == ['1', '2', '|', '3', '4', '|', '&']
+    assert query_processing.tokenized_query_to_poliz(
+        ['(', '!', '(', '1', '|', '2', ')', '&', '3', ')', '|', '4'])\
+        == ['1', '2', '|', '!', '3', '&', '4', '|']
+
+
+def test_find_doc_ids():
+    idx = {
+        '1': [1, 2, 3, 4],
+        '2': [1, 4, 5, 6],
+        '3': [3, 4, 7, 8],
+        '4': [7, 8, 9, 10]
+    }
+    all_docs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+    assert query_processing.find_doc_ids(
+        ['1', '2', '3', '&', '&'], idx, all_docs) == {4}
+    assert query_processing.find_doc_ids(
+        ['1', '2', '|', '3', '4', '|', '&'], idx, all_docs) == {3, 4}
+    assert query_processing.find_doc_ids(
+        ['1', '2', '|', '!', '3', '&', '4', '|'], idx, all_docs)\
+        == {7, 8, 9, 10}
