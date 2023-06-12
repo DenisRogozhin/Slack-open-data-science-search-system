0e3268fc0e48682d4b7b59e770481dde877f915d
Воронцов Александр Алексеевич <aavorontsov@domclick.ru>
2023-06-12 21:21:48 +0300

    Add base construction (DenisRogozhin#18)

--- src/parser/downloader.py
+++ src/parser/downloader.py
@@ -1,5 +1,7 @@
 import gdown
 
-url = 'https://drive.google.com/file/d/1n8sEgL1jDYxhDg5hLCs52okNmc3y7Kpq/view?usp=sharing'
-output = 'data/data.csv'
-gdown.download(url, output, quiet=False)
+
+if __name__ == "__main__":
+    url = 'https://drive.google.com/file/d/1n8sEgL1jDYxhDg5hLCs52okNmc3y7Kpq/view?usp=sharing'
+    output = 'data/data.csv'
+    gdown.download(url, output, quiet=False)
