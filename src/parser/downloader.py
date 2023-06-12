f95415c8c3058ad8df7505dd29bf460a794a1ebb
Воронцов Александр Алексеевич <aavorontsov@domclick.ru>
2023-06-12 21:19:09 +0300

    Add link and path (DenisRogozhin#18)

--- src/parser/downloader.py
+++ src/parser/downloader.py
@@ -1,5 +1,5 @@
 import gdown
 
-url = ''
-output = ''
+url = 'https://drive.google.com/file/d/1n8sEgL1jDYxhDg5hLCs52okNmc3y7Kpq/view?usp=sharing'
+output = 'data/data.csv'
 gdown.download(url, output, quiet=False)
